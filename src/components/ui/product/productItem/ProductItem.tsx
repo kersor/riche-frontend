ae8c5847b6b0f10409bcc9ee66c57bf1cf453a10
svolochyo <vasilev.rustik.03@mail.ru>
2025-10-19 16:44:12 +0300

    add animations for product items

--- src/components/ui/product/productItem/ProductItem.tsx
+++ src/components/ui/product/productItem/ProductItem.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './styles.module.css'
 import Image from 'next/image'
 import Button from '../../shared/button/Button'
 import { IProduct } from '@/types/catalog'
+import clsx from 'clsx'
 
 interface Props {
   product: IProduct
@@ -11,10 +14,48 @@ interface Props {
 const ProductItem = ({
   product
 }: Props) => {
+  const [isHover, setIsHover] = useState({
+    name: false,
+    image: false
+  })
+
+  const nameRef = useRef<HTMLDivElement | null>(null)
+  const imageRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const refName = nameRef.current
+    const refImage = imageRef.current
+
+    if (!refName || !refImage) return
+
+    const funcMouseEnterName = () => setIsHover(prev => ({...prev, name: true, image: true}))
+    const funcLeaveEnterName = () => setIsHover(prev => ({...prev, name: false, image: false}))
+    const funcMouseEnterImage = () => setIsHover(prev => ({...prev, name: true, image: false}))
+    const funcLeaveEnterImage = () => setIsHover(prev => ({...prev, name: false, image: false}))
+
+    refName.addEventListener('mouseenter', funcMouseEnterName)
+    refName.addEventListener('mouseleave', funcLeaveEnterName)
+
+    refImage.addEventListener('mouseenter', funcMouseEnterImage)
+    refImage.addEventListener('mouseleave', funcLeaveEnterImage)
+
+    return () => {
+      refName.removeEventListener('mouseenter', funcMouseEnterName)
+      refName.removeEventListener('mouseleave', funcLeaveEnterName)
+
+      refImage.removeEventListener('mouseenter', funcMouseEnterImage)
+      refImage.removeEventListener('mouseleave', funcLeaveEnterImage)
+    }
+  }, [])
+
   return (
     <div className={styles.product}>
         <div className={styles.product_main}>
-          <div className={styles.product_image}>
+          <div ref={imageRef} className={styles.product_image}>
+              <div className={clsx(
+                styles.product_back,
+                isHover.image && styles.product_back__hovered,
+              )} />
               <Image
                   src={product.image}
                   alt={product.image}
@@ -22,7 +63,10 @@ const ProductItem = ({
               />
           </div>
 
-          <div className={styles.product_name}>
+          <div ref={nameRef} className={clsx(
+            styles.product_name,
+            isHover.name ? 'opacity-60' : 'opacity-100'
+          )}>
             {product.name}
           </div>
 
